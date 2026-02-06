ab6cb072f243ffda508788be9956b20014c1fac2
Basant Joshi <basantjoshi6363@gmail.com>
2026-02-06 11:28:53 +0545

    feat: add order model with schema for managing orders

---
+++ backend/src/model/order.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    orderItems: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+            quantity: { type: Number, required: true },
+            price: { type: Number, required: true },
+        },
+    ],
+    shippingAddress: {
+        addressLine1: { type: String, required: true },
+        addressLine2: { type: String },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    paymentMethod: { type: String, required: true },
+
+    itemsPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+
+    shippingPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+}, { timestamps: true });
+
+export default mongoose.model('Order', orderSchema);
